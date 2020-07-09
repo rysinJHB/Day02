c932d921fe1c523b3559c67cd932f285a253d014
Ryan Sin <rysin@c4r11s6.wethinkcode.co.za>
2020-07-09 14:34:20 +0200

    Second commit

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void     ft_putchar(char c);
+
+void     ft_print_alphabet(void) {
+	char    start;
+    char	end;
+
+
+	start = 'z';
+	end = 'a';
+	while (start >= end) {
+		ft_putchar(start);
+		start--;
+	}
+
+}
+
+void    ft_putchar(char c) {
+	write(1, &c, 1);
+}
+
+int     main(void) {
+	ft_print_alphabet();
+	return(0);
+}
+
+
