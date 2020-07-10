9a3cf1907aefc3d070e3bec1c5d6c6939471e917
Ryan Sin <rysin@c5r8s5.wethinkcode.co.za>
2020-07-10 13:30:01 +0200

    Fifth commit

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+int		ft_putchar(char c)
+{
+  write (1, &c, 1);
+  return(0);
+}
+void	ft_print_var(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a != '7')
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print_var(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+void main ()
+{
+  ft_print_comb();
+  return(0);
+}
